9267eb18e4bff7a43fc32b74524028c26fb3e11c
peterbuild <peterbuild@gmail.com>
2017-11-27 00:14:02 +0200

    end task 1

--- src/task_1.java
+++ src/task_1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class task_1 {
@@ -20,10 +21,28 @@ public class task_1 {
             max = Math.max(max, array[i]); //опр мин.
             if (array[i]==5) rep5++; // считаем повторение 5
         }
-        //выводим 
+
+        //Arrays.sort(array); //Сортировка
+
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < size; i++)
+            System.out.print(array[i] + " "); //Выводим масив в консоль
+        System.out.println();
+
+
+        //выводим
         System.out.println("Min. element = " + min);
         System.out.println("Max. element = " + max);
         System.out.println("Repeat 5     = " + rep5);
-
     }
 }
