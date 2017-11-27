12670c84225a52ad540ceac6b722aa142047d1d9
peterbuild <peterbuild@gmail.com>
2017-11-27 05:40:11 +0200

    add practic task 5

--- src/practic_task_5.java
+++ src/practic_task_5.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class practic_task_5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter width :");
+        int w = in.nextInt();
+        System.out.println("Enter height :");
+        int h = in.nextInt();
+        for (int i = 0; i < w; i++){
+            for (int j = 0; j < h; j++){
+                System.out.print("+"); //рисуем ряд
+            }
+            System.out.println(); //новый ряд
+        }
+    }
 }
